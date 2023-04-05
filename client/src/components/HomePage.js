83b83ba4ae29ec589bfa6159c88dbccf6f472a84
test <test@tests-MacBook-Pro.local>
2023-04-04 21:22:20 -0400

    adding countries list

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,28 +1,62 @@
 import image from "../images/BannerImage.png";
 import styledComponents from "styled-components";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 const HomePage = () => {
+
+  const [country1, setCountry1] = useState("");
+  const [countries, setCountries] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/independent?status=true")
+    .then((res) => res.json())
+    .then((data) => { 
+      // data.map((country) => {
+      //   setCountry1(country.name.official)})})
+      setCountries(data);})
+        
+  }, []);
+    
   return (
+    <>
     <SearchDiv>
       <BannerImage src={image} alt="logo" />
       <Form>
-        <label htmlFor="search">Enter country name:</label>
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Enter country name:" />
+        <Button type="submit">Search</Button>
       </Form>
     </SearchDiv>
+    <h1>Countries</h1>
+    <CountryList>
+      
+    {console.log(countries)}
+      
+    </CountryList></>
   );
 };
+
+const CountryList = styledComponents.div`
+display: flex;
+flex-direction: ;
+align-items: center;
+`
 const BannerImage = styledComponents.img`
     width: 30%;
     height: 30%;
     left: 35vw;
     top: 5vh;
     position: relative;
-    padding: 70px 0 40px;
+    margin: 70px 0 40px;
     `;
 const SearchDiv = styledComponents.div`
     display: flex;
     flex-direction: column;
     position: relative;`;
+
+const Button = styledComponents.button`
+margin-left: 5px;
+`
 
 const Form = styledComponents.form`
     display: flex;
