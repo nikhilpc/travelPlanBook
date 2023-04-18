69fedea42bbf57948c36ae54059d239fdb55309d
Nikhil <35919985+nikhilpc@users.noreply.github.com>
2023-04-17 20:27:32 -0400

    fixing navigation bug

--- client/src/components/SearchingPage.js
+++ client/src/components/SearchingPage.js
@@ -5,20 +5,29 @@ import { useState, useEffect } from "react";
 import Profile from "./Profile";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
-import SearchBar from "./SearchBar";
 
 const SearchingPage = () => {
     const [country, setCountry] = useState("");
+    const [country1, setCountry1] = useState("")
     const [countries, setCountries] = useState([]);
 
     const countrySelected = (e) => {
         setCountry(e.target.textContent);
+        setCountry1(e.target.textContent);
+        setSearchTerm(e.target.textContent);
     };
 
     const navigateToBlog = (e) => {
         e.preventDefault();
+
         if (country) {
             window.location.href = `/blog/${country}`;
+        } else {
+            window.location.href = "/search";
+        }
+
+        if (country1) {
+            window.location.href = `/blog/${country1}`;
         } else {
             window.location.href = "/search";
         }
@@ -32,27 +41,64 @@ const SearchingPage = () => {
             });
     }, []);
 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+    const fetchSuggestions = (value) => {
+        const suggestions = ['Barbados', 'Suriname', 'Namibia', 'Guinea', 'Vanuatu', 'Samoa', 'France', 'Andorra', 'Azerbaijan', 'Maldives', 'Saint Lucia', 'Panama', 'Timor-Leste', 'North Macedonia', 'Denmark', 'Egypt', 'Estonia', 'Czechia', 'Bahamas', 'Uruguay', 'Comoros', 'Costa Rica', 'Togo', 'São Tomé and Príncipe', 'Netherlands', 'Australia', 'Malaysia', 'Nepal', 'Cuba', 'North Korea', 'Moldova', 'Zambia', 'Dominica', 'Marshall Islands', 'Tonga', 'Peru', 'Cape Verde', 'Kiribati', 'Finland', 'Ivory Coast', 'Pakistan', 'Djibouti', 'Micronesia', 'Slovenia', 'Kyrgyzstan', 'Switzerland', 'Kuwait', 'Seychelles', 'United Kingdom', 'Fiji', 'Yemen', 'Central African Republic', 'Belgium', 'Argentina', 'Madagascar', 'South Africa', 'Latvia', 'Zimbabwe', 'Gabon', 'Romania', 'Germany', 'Bangladesh', 'Jordan', 'Mozambique', 'Israel', 'Eritrea', 'Indonesia', 'Japan', 'Spain', 'Afghanistan', 'United Arab Emirates', 'Malawi', 'Turkey', 'Belarus', 'Montenegro', 'Bosnia and Herzegovina', 'Eswatini', 'Lithuania', 'Turkmenistan', 'Ethiopia', 'Hungary', 'Chad', 'Nigeria', 'San Marino', 'Ukraine', 'Haiti', 'Botswana', 'Portugal', 'Guyana', 'Ireland', 'Mauritius', 'Saint Kitts and Nevis', 'Iceland', 'Chile', 'Kazakhstan', 'China', 'Armenia', 'Bolivia', 'Sudan', 'Vietnam', 'Burundi', 'Canada', 'Colombia', 'Lesotho', 'Guatemala', 'Uzbekistan', 'Senegal', 'Gambia', 'Liechtenstein', 'Tuvalu', 'Equatorial Guinea', 'Bhutan', 'Paraguay', 'Grenada', 'Papua New Guinea', 'Jamaica', 'Republic of the Congo', 'Poland', 'Russia', 'Mauritania', 'Burkina Faso', 'DR Congo', 'Benin', 'Uganda', 'Mongolia', 'Laos', 'Algeria', 'Brunei', 'Kenya', 'Sri Lanka', 'Dominican Republic', 'Austria', 'Luxembourg', 'Georgia', 'Slovakia', 'Norway', 'Thailand', 'India', 'Morocco', 'Nicaragua', 'Qatar', 'Brazil', 'New Zealand', 'Singapore', 'Syria', 'Belize', 'Venezuela', 'Bahrain', 'Cameroon', 'Cyprus', 'United States', 'Angola', 'Tunisia', 'Monaco', 'Rwanda', 'Trinidad and Tobago', 'Malta', 'Mexico', 'Antigua and Barbuda', 'South Korea', 'Niger', 'Albania', 'Somalia', 'Liberia', 'Myanmar', 'Tanzania', 'Iraq', 'Saint Vincent and the Grenadines', 'Libya', 'Sierra Leone', 'Serbia', 'Ghana', 'South Sudan', 'Sweden', 'Greece', 'Philippines', 'Guinea-Bissau', 'Saudi Arabia', 'Palau', 'Bulgaria', 'Nauru', 'Cambodia', 'Italy', 'Mali', 'Iran', 'Tajikistan', 'Oman', 'El Salvador', 'Lebanon', 'Vatican City', 'Ecuador', 'Croatia', 'Solomon Islands', 'Honduras']
+            .filter(suggestion => suggestion.toLowerCase().startsWith(value.toLowerCase()));
+        setSuggestions(suggestions);
+    }
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setSearchTerm(value);
+
+        // Fetch suggestions based on the input value
+        fetchSuggestions(value);
+    }
+
+    const handleSuggestionClick = (suggestion) => {
+        setSearchTerm(suggestion);
+        setCountry1(suggestion)
+        setSuggestions([]);
+    }
     return (
         <div>
             <Headers>
+                <LoginButton />
+                <LogoutButton />
                 <MainHeading>Budget Travel Guide</MainHeading>
                 <AboutUs to="/about">About Us</AboutUs>
 
             </Headers>
-            <LoginButton />
-            <LogoutButton />
+
             <Profile >Profile</Profile>
             <SearchDiv>
                 <BannerImage src={image} alt="logo" />
 
-                <DivSearch>
+                <Division>
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={handleChange}
+                        placeholder="Enter Country Name"
+                        autoFocus
+                    />
+                    {suggestions.length > 0 && (
+                        <UL>
+                            {suggestions.map(suggestion => (
+                                <LI key={suggestion} onClick={() => handleSuggestionClick(suggestion)}>
+                                    {suggestion}
+                                </LI>
+                            ))}
+                        </UL>
+                    )}
 
-                    <SearchBar />
 
                     <Button type="submit" onClick={(e) => navigateToBlog(e)}>
                         Search
                     </Button>
-                </DivSearch>
+                </Division>
             </SearchDiv>
             <H1>List of Countries</H1>
             <CountryList>
@@ -65,63 +111,89 @@ const SearchingPage = () => {
         </div>
     )
 }
+
+
+const LI = styledComponents.li`
+:hover {
+    background-color: yellow;
+    cursor: pointer;
+    border: 1px solid red;
+    font-weight: bold;
+}
+list-style-type: none
+
+`
+const UL = styledComponents.ul`
+background-color: orange;
+padding: 10px;
+color: black;
+`
+const Division = styledComponents.div`
+position: relative
+display: flex
+`
+
 const Headers = styledComponents.div`
+
 display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+        justify-content: space-between;
+    -webkit-box-align: center;
 `;
 
 const MainHeading = styledComponents.h1`
-    font-size: 50px;
-    position:relative;
-    left: 35vw;
-    color: Orange;
-    `;
+font - size: 50px;
+position: relative;
+color: Orange;
+`;
 const H1 = styledComponents.h1`
-    font-size: 12px;
-    padding-left: 10px;
-    `;
+font - size: 12px;
+padding - left: 10px;
+`;
 const CountryButton = styledComponents.button`
-    font-size: 8px;
-    padding: 10px;
-    border: 1px solid black;
-    border-radius: 5px;
-    margin: 10px;
+font - size: 8px;
+padding: 10px;
+border: 1px solid black;
+border - radius: 5px;
+margin: 10px;
 `;
 
 const AboutUs = styledComponents(Link)`
-margin-right: 30px;
-text-decoration: none;
+margin - right: 30px;
+text - decoration: none;
 `;
 const CountryList = styledComponents.div`
 display: flex;
-flex-wrap: wrap;  
-max-width: 95vw;
+flex - wrap: wrap;
+max - width: 95vw;
 padding: 0 20px;
-justify-content: center;
+justify - content: center;
 `;
 const BannerImage = styledComponents.img`
-    width: 30%;
-    height: 30%;
-    left: 35vw;
-    top: 5vh;
-    position: relative;
-    margin-bottom:40px;
-    `;
+width: 30 %;
+height: 30 %;
+position: relative;
+margin - bottom: 40px;
+`;
 const SearchDiv = styledComponents.div`
     display: flex;
+    position: relative;
     flex-direction: column;
-    position: relative;`;
+    align-items: center;
+`;
+
 
 const Button = styledComponents.button`
-margin-left: 5px;
+margin - left: 5px;
 `;
 
 const DivSearch = styledComponents.div`
-    margin-top: 10px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-   `;
+margin - top: 10px;
+display: flex;
+flex - direction: row;
+justify - content: center;
+
+`;
 export default SearchingPage
